421136a7c47b97bd803ae7593e4405abea36f92b
William Navarro <142845725+anonimogxh@users.noreply.github.com>
2024-09-29 23:50:44 -0300

    Add files via upload

---
+++ Troco/Program.cs
@@ -0,0 +1,14 @@
+﻿decimal vlcompra, vlpago, vltroco;
+
+Console.WriteLine("---Troco---\n");
+
+
+Console.Write("Valor de Compra:.....");
+vlcompra = Convert.ToDecimal (Console.ReadLine());
+
+Console.Write("Valor Pago:....");
+vlpago = Convert.ToDecimal (Console.ReadLine());
+
+vltroco = vlpago - vlcompra;
+
+Console.WriteLine($"\n Troco: {vltroco:C}");
